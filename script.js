0fcbe99f26561597815cd3d14e0738bf5e1e07be
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-06-25 01:03:24 +0000

    Jun 25, 2026, 1:03 AM

--- script.js
+++ script.js
@@ -2,5 +2,19 @@
 var userName;
 
 console.log("Hello world, welcome to my mini cafe!")
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+   const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameField");
+const PRODUCT_FIELD = document.getElementById("productField");
+const AGE_FIELD = document.getElementById("AgeField");
+const PRODUCT_PRICE_FIELD = document.getElementById("ProductPriceField");
 
+function getFormInput(){
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p> Your name is " + userName + "</p>";
+    let userProduct = PRODUCT_FIELD.value;
+    OUTPUT.innerHTML += "<p> Your product is " + userProduct + "</p>";
+    let userAge = AGE_FIELD.value;
+    OUTPUT.innerHTML += "<p> Your age is " + userAge + "</p>";
+    let userProductPrice = PRODUCT_PRICE_FIELD.value;
+    OUTPUT.innerHTML += "<p> Your product price is $" + userProductPrice + "</p>";
+}
