ba88df583a20bb95d6adc658f5ed9dab1dce81d9
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-07-20 21:35:39 +0000

    Jul 20, 2026, 9:35 PM

--- script.js
+++ script.js
@@ -2,11 +2,3 @@
 var userName;
 
 console.log("Hello world, welcome to my mini cafe!")
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-const NAME_FIELD = document.getElementById("nameField");
-
-
-function getFormInput(){
-    let userName = NAME_FIELD.value;
-    OUTPUT.innerHTML = "<p> Your name is " + userName + "</p>";
-   
