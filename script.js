65af304f77dac9813a1179be464d8360d59afca7
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-07-24 02:22:18 +0000

    Jul 24, 2026, 2:22 AM

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 console.log("Running script.js")
 console.log("Hello world, welcome to my mini cafe!")
 
-const ietms = { }
+const ietms = [
+    {name: "Fruit Cakes",Price:5},
+    {name: "Fruit Tart ",Price:3},
+    {name: "Creamy lamb pasta ",Price:20}
+
+]
